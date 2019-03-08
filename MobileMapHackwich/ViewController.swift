c87e115003e53931eb86d3c916ad44477c1f9630
zamin ahmed <zahmed@zamins-macbook-air.local>
2019-03-08 11:28:55 -0600

    Added a location to the map

--- MobileMapHackwich/ViewController.swift
+++ MobileMapHackwich/ViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+   
+    let barringtonAnnotation = MKPointAnnotation()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+    let latitude = 42.15704
+    let longitude = -88.14812
+    let coordinate = CLLocationCoordinate2DMake(latitude,longitude)
+    barringtonAnnotation.coordinate = coordinate
+    mapView.addAnnotation(barringtonAnnotation)
 }
 
+}
